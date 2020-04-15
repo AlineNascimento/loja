24fa8a42539fb38729248ed4862e3acfe52651f8
Aline Nascimento <alinephysics@gmail.com>
2020-04-14 21:05:14 -0300

    Criacao de formulario e realizacao de comunicacao com o banco

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,5 +4,9 @@ class ProdutosController < ApplicationController
         @produtos = Produto.order :nome
     end
 
+    def create
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+        produto = Produto.create valores
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "produtos" => "produtos#index" 
+  #get "/produtos" => "produtos#index" 
   # get "[o nome que queremos para a url]" => "[nome do Controller]#[nome da página html]"
-
+  post "/produtos" => "produtos#create"
+  root "produtos#index"  
+  #link rota da raiz da aplicacao
+  get "/produtos/new" => "produtos#new"
 
 end
